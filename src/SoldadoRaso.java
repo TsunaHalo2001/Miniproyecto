ea97b55a747a04601c5775cd873ea2d0aca0e3d1
TsunaHalo2001 <joan.villamil@correounivalle.edu.co>
2024-10-07 18:38:54 -0500

    Added all the classes with it's methods and a provisional behavior

--- src/SoldadoRaso.java
+++ src/SoldadoRaso.java
@@ -1,5 +1,10 @@
 public class SoldadoRaso extends Soldado {
 	public SoldadoRaso(String nombre, String id) {
-		super(nombre, id, "Soldado Raso");
+		super(nombre, id, RangoEnum.Soldado_Raso);
+	}
+
+	@Override
+	public void realizarAccion() {
+		System.out.println("Seguir ordenes de sus superiores");
 	}
 }
---
+++ src/Teniente.java
@@ -0,0 +1,21 @@
+public class Teniente extends Soldado {
+	protected String unidad;
+
+	public Teniente(String nombre, String id, String unidad) {
+		super(nombre, id, RangoEnum.Teniente);
+		this.unidad = unidad;
+	}
+
+	@Override
+	public void realizarAccion() {
+		System.out.println("Supervisar soldados rasos");
+	}
+
+	public String getUnidad() {
+		return unidad;
+	}
+
+	public void setUnidad(String unidad) {
+		this.unidad = unidad;
+	}
+}
