afb16ce452ea7f22c97e21dddf81e8c835bc90a0
TsunaHalo2001 <145141553+TsunaHalo2001@users.noreply.github.com>
2024-10-07 13:49:13 -0500

    Corrected App.java
    Erased test code

--- src/App.java
+++ src/App.java
@@ -1,6 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Soldado soldado1 = new Soldado("dilan", "1945", "raso");
-        soldado1.mostrarInformacion();
+        
     }
 }
